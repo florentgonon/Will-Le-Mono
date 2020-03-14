1fe992a6a4a22b8e247cc31e1d48d3f8f0a1f3d2
Florent GONON <florent.gonon33@gmail.com>
2020-03-14 12:25:42 +0100

    formspree DONE

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :admins
   root to: 'pages#home'
 
+  get 'pages/contact'
+  get 'pages/about'
+
   resources :articles, only: [:index, :show]
   resources :formations, only: [:index]
 
