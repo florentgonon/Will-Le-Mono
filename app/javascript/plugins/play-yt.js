5bce2b7fb7f1b3db0678bd67a52070ef1678bcc1
Florent GONON <florent.gonon33@gmail.com>
2020-03-26 14:10:46 -0400

    CSS begin

--- app/javascript/plugins/play-yt.js
+++ app/javascript/plugins/play-yt.js
@@ -1,12 +1,26 @@
 function playyt() {
   const frames = document.getElementById("frame");
   const btnnns = document.querySelectorAll('.yt-link-button');
+  const modal = document.getElementById("my-modal-frame-yt");
+
 
   btnnns.forEach((btn)=>{
     btn.addEventListener("click",(event) => {
+      modal.style.display = "flex";
       frame.src = event.target.dataset.src;
+
+
     });
   });
+
+
 }
+
+window.onclick = function(event) {
+    console.log(event.target);
+    if (event.target !== modal) {
+      modal.style.display = "none";
+    }
+  }
 
 export { playyt };
