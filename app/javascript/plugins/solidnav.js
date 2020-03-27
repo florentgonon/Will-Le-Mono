29f6c1b1310ec1baf82abbe0a675e6fe5102f30d
Florent GONON <florent.gonon33@gmail.com>
2020-03-27 08:29:38 -0400

    continue front

--- app/javascript/plugins/solidnav.js
+++ app/javascript/plugins/solidnav.js
@@ -1,6 +1,6 @@
 jQuery(function($) {
   $(window).scroll(function(){
-      if($(window).scrollTop() > 250) {
+      if($(window).scrollTop() > 150) {
             $('.navbar').addClass("solid-nav");
       } else {
             $('.navbar').removeClass("solid-nav");
