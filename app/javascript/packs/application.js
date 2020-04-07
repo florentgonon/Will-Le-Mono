e781fc3621c55e976a8dbcf1f999a70ef7f4dc73
Florent GONON <florent.gonon33@gmail.com>
2020-04-06 20:56:39 -0400

    implementation preloader DONE

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,11 @@ import "../plugins/solidnav.js";
 import "../plugins/caroussel.js";
 import "../plugins/play-yt.js";
 import "../plugins/btn_scrollup.js";
+import "../plugins/preloader.js";
 
 import { formspree } from "plugins/formspree.js";
 import { playyt } from "plugins/play-yt.js";
 
 formspree();
 playyt();
+
