739b832a1f7b98c7fc9d5386ff13e6e98013c4f0
Florent GONON <florent.gonon33@gmail.com>
2020-04-08 18:57:59 -0400

    modal mail DONE

---
+++ app/javascript/plugins/captation-mail.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+//loads when document is ready
+
+if (document.cookie.indexOf('modal_shown=') >= 0) {
+ //do nothing if modal_shown cookie is present
+} else {
+  setTimeout(function() {
+    $('.mail').modal();
+  }, 15000);
+  document.cookie = 'modal_shown=seen'; //set cookie modal_shown
+  //cookie will expire when browser is closed
+}
+
+})
+
+// setTimeout(function() {
+//     $('.mail').modal();
+//   }, 1000);
